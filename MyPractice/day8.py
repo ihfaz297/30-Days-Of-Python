c67fb023e378c09659dbdf102ddaa9f372cd4c26
ihfaz297 <adib000297@gmail.com>
2026-05-16 01:13:28 +0600

    uwu

---
+++ MyPractice/day8.py
@@ -0,0 +1,25 @@
+employee_1_details = {
+    "name": "John Doe",
+    "age" : 30,
+    "contact": "+8801766866902"
+}
+print(employee_1_details["contact"])
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_married':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+
+print(person["address"]["street"])
+
+print(person.keys())
+
+####bakita sylhet eshe korbo, ekhn jaaan call dibe
