bd30fba6e21c764c1fa78748824b1f4012ed839c
IHFAZ AHMED ADIB <167864965+ihfaz297@users.noreply.github.com>
2026-05-16 20:42:06 +0000

    ok

---
+++ MyPractice/day10.py
@@ -0,0 +1,17 @@
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skill': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+
+if('skills' in person):
+    print(person['skills'])
+else:
+    print("oops")
